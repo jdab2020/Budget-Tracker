0181a2cce07e5f87f3f73353da0ca713eae1956d
jdab2020 <jdabalos90@gmail.com>
2020-03-16 15:27:02 -0700

    deploying

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-console.log("trying to push")
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
